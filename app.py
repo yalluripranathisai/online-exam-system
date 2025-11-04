1d63fc8dc69b67e3d49500a0ea2d7ac54d7cc849
pranathi sai <pranathiyalluri@gmail.com>
2025-11-05 00:38:18 +0530

    Updated host and port for Railway deployment

--- app.py
+++ app.py
@@ -522,8 +522,5 @@ def download_student_report(username):
 def home():
     return "Online Examination System Running Successfully!"
 
-
-if __name__ == '__main__':
-    # Render provides the port via environment variable
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
